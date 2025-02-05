b390276f5d9031e3040861fb5c37d1c5478299ec
Karam <karam.zako07@gmail.com>
2025-02-04 17:50:57 -0800

    basic

---
+++ src/main/java/com/karam/dentistry/customer/TableActionCellRender.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.karam.dentistry.customer;
+
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author Karam
+ */
+public class TableActionCellRender extends DefaultTableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        PanelAction action = new PanelAction();
+        return action;
+    }
+    
+}
